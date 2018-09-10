17cfaf2ccc27dff05c6ffc42444c56226b5e1629
Rowan Collins <rowan.collins@netsells.co.uk>
2018-09-10 15:42:52 +0100

    initial commit

---
+++ src/js/components/TransitionExpandDown.vue
@@ -0,0 +1,26 @@
+<template>
+    <transition name="expandDown">
+        <slot/>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: 'transition-expand-down',
+    };
+</script>
+
+<style lang="scss">
+    .expandDown-enter-active,
+    .expandDown-leave-active {
+        transition: all 0.5s;
+        max-height: 999px;
+
+    }
+    .expandDown-enter,
+    .expandDown-leave-to {
+        opacity: 0;
+        max-height: 0;
+
+    }
+</style>
