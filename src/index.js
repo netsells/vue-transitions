5c57852205063481571bff4b9d3a573643cd17cc
Rowan Collins <rowan.collins@netsells.co.uk>
2018-10-05 19:00:57 +0100

    fix exports and add fade down transition

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-import TransitionsExpandDown from './js/components/TransitionExpandDown';
-
-export default {
-    TransitionsExpandDown,
-};
+export { default as TransitionExpandDown } from './js/components/TransitionExpandDown';
+export { default as TransitionFadeDown } from './js/components/TransitionFadeDown';
---
+++ src/js/components/TransitionFadeDown.vue
@@ -0,0 +1,28 @@
+<template>
+    <transition name="fade-down">
+        <slot/>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: 'transition-fade-down',
+    };
+</script>
+
+<style lang="scss">
+    .fade-down-enter-active,
+    .fade-down-leave-active {
+        transition: 0.2s ease;
+    }
+    .fade-down-enter,
+    .fade-down-leave-to {
+        opacity: 0;
+    }
+    .fade-down-leave-to {
+        transform: translateY(10px);
+    }
+    .fade-down-enter {
+        transform: translateY(-10px);
+    }
+</style>
